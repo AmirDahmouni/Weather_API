2aad9cf0522b27ec79616d87441693162dd066fc
Amir <dahmouni_amir@hotmail.fr>
2023-09-04 20:11:51 +0100

    fix/order_cruds

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,11 @@ const userController = require("../controllers/User")
 const error = require("../middlewares/error")
 const router = express.Router();
 
-router.put('/rmfavoris', isAuth, userController.removefavoris, error);
-router.put('/addfavoris', isAuth, userController.addfavoris, error);
 router.post('/signin', userController.signin, error);
 router.post('/signup', userController.register, error);
+router.put('/rmfavoris', isAuth, userController.removefavoris, error);
+router.put('/addfavoris', isAuth, userController.addfavoris, error);
+
 
 
 module.exports = router;
